f6fc1e583df1e441e57403296ec3b228d2da4f41
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2016-03-07 14:11:15 +0100

    URL to twitter search page fixed.

--- view.php
+++ view.php
@@ -152,7 +152,7 @@ echo html_writer::table($table);
 // insert timeline
 if (isset($tcount->widget_id)) {
     ?>
-    <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=<?php echo $tcount->hashtag; ?>" data-widget-id="<?php echo $tcount->widget_id; ?>">Tweets sobre <?php echo $tcount->hashtag; ?></a>
+    <a class="twitter-timeline" data-dnt="true" target="_blank" href="https://twitter.com/search?q=<?php echo urlencode($tcount->hashtag); ?>" data-widget-id="<?php echo $tcount->widget_id; ?>">Tweets sobre <?php echo $tcount->hashtag; ?></a>
     <script>!function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location)?'http':'https'; if (!d.getElementById(id)){js = d.createElement(s); js.id = id; js.src = p + "://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs); }}(document, "script", "twitter-wjs");</script>
     <?php
 }
