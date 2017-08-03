d9b0ec31a580c4259a70be7ec94650cca36d2fe4
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2017-08-03 08:54:51 +0200

    JS paths for graphviz visualization.

--- view/graph/graphviz.php
+++ view/graph/graphviz.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+/* ***************************
+ * Module developed at the University of Valladolid
+ * Designed and directed by Juan Pablo de Castro at telecommunication engineering school
+ * Copyright 2017 onwards EdUVaLab http://www.eduvalab.uva.es
+ * @author Juan Pablo de Castro
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package msocial
+ * *******************************************************************************
+ */
 use mod_msocial\connector\social_interaction;
 use Graphp\GraphViz\Dot;
 require_once ('socialgraph.php');
@@ -38,8 +61,9 @@ $dot = new Dot();
 $graph = $socialgraph->get_graph();
 $graph->getAttributeBag()->setAttribute('graphviz.graph.rankdir', 'LR');
 $dotsource = $dot->getOutput($graph);
-
+/** @var page_requirements_manager $reqs */
 $reqs->js('/mod/msocial/view/graph/js/configuregraphvizrequire.js', false);
+global $CFG;
 $reqs->js_call_amd('msocialview/graphviz', 'initview', ['#graph', '#dot_src']);
 /* @var $OUTPUT \core_renderer */
 echo $OUTPUT->container('', '', 'graph');
--- view/graph/js/configuregraphvizrequire.js
+++ view/graph/js/configuregraphvizrequire.js
@@ -1,11 +1,14 @@
+var url = window.location.href;
+var baseurl = url.substring(0,url.indexOf('/mod/msocial'));
+
 requirejs.config({
     paths: {
-    	d3: '/mod/msocial/view/graph/js/d3/d3.v3',
-		"dot-checker": '/mod/msocial/view/graph/js/graphviz-d3-renderer/dist/dot-checker',
-		"layout-worker": '/mod/msocial/view/graph/js/graphviz-d3-renderer/dist/layout-worker',
-		worker: '/mod/msocial/view/graph/js/requirejs-web-workers/src/worker',
-		renderer: '/mod/msocial/view/graph/js/graphviz-d3-renderer/dist/renderer',
-		'msocialview/graphviz': '/mod/msocial/view/graph/js/graphviz',
+    	d3: baseurl+'/mod/msocial/view/graph/js/d3/d3.v3',
+		"dot-checker": baseurl+'/mod/msocial/view/graph/js/graphviz-d3-renderer/dist/dot-checker',
+		"layout-worker": baseurl+'/mod/msocial/view/graph/js/graphviz-d3-renderer/dist/layout-worker',
+		worker: baseurl+'/mod/msocial/view/graph/js/requirejs-web-workers/src/worker',
+		renderer: baseurl+'/mod/msocial/view/graph/js/graphviz-d3-renderer/dist/renderer',
+		'msocialview/graphviz': baseurl+'/mod/msocial/view/graph/js/graphviz',
     },
     waitSeconds: 20
 });
