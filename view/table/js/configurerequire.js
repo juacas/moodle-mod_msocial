f1c31e1f1f9c87fc693576c71c7a49b9fdfd6479
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2017-08-04 10:51:03 +0200

    Calculate js base url for requirejs.

--- view/table/js/configurerequire.js
+++ view/table/js/configurerequire.js
@@ -1,12 +1,14 @@
+var url = window.location.href;
+var baseurl = url.substring(0,url.indexOf('/mod/msocial'));
 requirejs.config({
     paths: {
-        "msocialview/table": '/mod/msocial/view/table/js/viewlib',
-        "datatables.net": '/mod/msocial/view/table/js/dataTables',
-        "datatables.colReorder": '/mod/msocial/view/table/js/dataTables.colReorder',
-        "datatables.fixedHeader": '/mod/msocial/view/table/js/dataTables.fixedHeader',
-        "datatables.responsive": '/mod/msocial/view/table/js/dataTables.responsive',
-        "datatables.net-buttons": '/mod/msocial/view/table/js/dataTables.buttons',
-        "datatables.colVis": '/mod/msocial/view/table/js/buttons.colVis',
+        "msocialview/table": baseurl+'/mod/msocial/view/table/js/viewlib',
+        "datatables.net": baseurl+'/mod/msocial/view/table/js/dataTables',
+        "datatables.colReorder": baseurl+'/mod/msocial/view/table/js/dataTables.colReorder',
+        "datatables.fixedHeader": baseurl+'/mod/msocial/view/table/js/dataTables.fixedHeader',
+        "datatables.responsive": baseurl+'/mod/msocial/view/table/js/dataTables.responsive',
+        "datatables.net-buttons": baseurl+'/mod/msocial/view/table/js/dataTables.buttons',
+        "datatables.colVis": baseurl+'/mod/msocial/view/table/js/buttons.colVis',
     },
     waitSeconds: 20
 });
