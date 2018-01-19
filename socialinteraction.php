3a1508faf04b20130806d927f4477aa1714a2a42
Juan Pablo de Castro <jpdecastro@tel.uva.es>
2018-01-19 11:54:08 +0100

    Emoji filter

--- socialinteraction.php
+++ socialinteraction.php
@@ -218,7 +218,12 @@ class social_interaction {
         }
         $DB->insert_records('msocial_interactions', $records);
     }
-
+/**
+ * Remove emojis in utf8mb4 format.
+ * https://stackoverflow.com/questions/12807176/php-writing-a-simple-removeemoji-function
+ * @param string $text
+ * @return NULL|string the filtered text.
+ */
     static private function clean_emojis($text) {
         if ($text == null) {
             return null;
@@ -233,6 +238,8 @@ class social_interaction {
             // Match Transport And Map Symbols
             $regextransport = '/[\x{1F680}-\x{1F6FF}]/u';
             $cleantext = preg_replace($regextransport, '', $cleantext);
+            $cleantext = preg_replace('/([0-9|#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $cleantext);
+
             return $cleantext;
         }
     }
